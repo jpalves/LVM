665f4ce1383f8c070816dffee5ddb22ea4b1be4d
João Pedro Alves <jpalves@home-linux.(none)>
2013-06-04 21:50:06 +0100

    Resumo de um Fórum

---
+++ blocks/lang/en/block_newsforuns.php
@@ -0,0 +1,7 @@
+<?php
+$string['pluginname'] = 'Forum Summary';
+$string['blocktitle'] = 'Block Title';
+$string['nomeforuns'] = 'Forum Name';
+$string['itens']      = 'Number of Items';
+$string['erroforum']  = 'Forum misconfigured';
+?>
---
+++ blocks/lang/pt/block_newsforuns.php
@@ -0,0 +1,7 @@
+<?PHP
+$string['pluginname'] = 'Resumo do Fórum';
+$string['blocktitle'] = 'Título do bloco';
+$string['nomeforuns'] = 'Nome do Fórum';
+$string['itens']      = 'Número de Itens';
+$string['erroforum']  = 'Fórum mal configurado';
+?>
